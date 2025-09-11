b2ff0cb176fbfe3be1b9909f29fc88ff8ebce1d5
BrandosFotos <bjwhite97@gmail.com>
2025-09-10 22:06:37 -0400

    Added FuzzyFind command to init.lua

--- init.lua
+++ init.lua
@@ -2,3 +2,6 @@ require("config.lazy")
 
 vim.o.termguicolors = true
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+
